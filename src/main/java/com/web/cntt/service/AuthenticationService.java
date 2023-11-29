1a3af8eb772abfc8fcbf1d41e82c74392696de95
Bao le <baole13122001@gmail.com>
2023-11-29 22:41:30 +0700

    fix bug Security

--- src/main/java/com/web/cntt/service/AuthenticationService.java
+++ src/main/java/com/web/cntt/service/AuthenticationService.java
@@ -1,17 +1,17 @@
-//package com.web.cntt.service;
-//
-//import com.web.cntt.repository.UserRepository;
-//import io.jsonwebtoken.Jwt;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//@RequiredArgsConstructor
-//public class AuthenticationService {
-//    private final PasswordEncoder passwordEncoder;
-//    private final UserRepository userRepository;
-//    private final JwtService jwtService;
-//    private final AuthenticationManager authenticationManager;
-//}
+package com.web.cntt.service;
+
+import com.web.cntt.repository.UserRepository;
+import io.jsonwebtoken.Jwt;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+}
