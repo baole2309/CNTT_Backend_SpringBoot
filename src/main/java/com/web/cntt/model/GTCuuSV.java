081681199c9f0fd04582e2b578dda3498639c1ab
Bao le <baole13122001@gmail.com>
2023-11-30 20:26:50 +0700

    fix bug

--- src/main/java/com/web/cntt/model/GTCuuSV.java
+++ src/main/java/com/web/cntt/model/GTCuuSV.java
@@ -5,12 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
 
-import java.io.Serial;
-import java.util.Date;
+import java.sql.Date;
 import java.util.UUID;
 
 @Data
@@ -26,9 +24,9 @@ public class GTCuuSV extends BaseEntity {
     @JdbcTypeCode(SqlTypes.VARCHAR)
     private UUID id;
     private String title;
-    @Column(length = 10000)
+    @Column(length = 3000)
     private String description;
-    @Column(length = 10000)
+    @Column(length = 3000)
     private String aim;
     private String image;
     private Date ngayTB;
